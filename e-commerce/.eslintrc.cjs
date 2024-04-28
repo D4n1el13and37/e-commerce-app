d31bc528a32fccac0377bec696287a9c94b11721
D4n1el13and37 <daniel837@mail.ru>
2024-04-28 19:14:31 +0300

    init: e-commerce task, configure environment, add scripts

---
+++ e-commerce/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb-base',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'lines-between-class-members': 'off',
+    'import/extensions': 'off',
+  },
+};

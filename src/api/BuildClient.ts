1447dd644d120b5599551f1a13017f4f699173a9
Anna <cainowa@gmail.com>
2024-05-03 17:13:20 +0200

    feat: add set-up TS SDK

--- src/api/BuildClient.ts
+++ src/api/BuildClient.ts
@@ -1,70 +1,31 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch'
+import { ClientBuilder, Client, AuthMiddlewareOptions, HttpMiddlewareOptions } from '@commercetools/sdk-client-v2';
+import { createApiBuilderFromCtpClient, ApiRoot } from '@commercetools/platform-sdk';
 
-import {
-  ClientBuilder,
-  createHttpClient,
-  createClient,
-  createAuthForClientCredentialsFlow,
-  createAuthForPasswordFlow,
-
-  // Import middlewares
-  type AuthMiddlewareOptions, // Required for auth
-  type HttpMiddlewareOptions, // Required for sending HTTP requests
-} from '@commercetools/sdk-client-v2';
-
-import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
-
-export const projectKey = 'yes-to-plants';
-const clientId = 'Qk9DVzS_Wl01PGjWViCUPII9';
-const clientSecret = 'itkhP2kwM2U8QwgdAtMVg5p89YiWJzRW';
-const region = 'europe-west1.gcp';
-const scopes = ['manage_project:yes-to-plants'];
-
-function getClient() {
-  const authMiddleware = createAuthForClientCredentialsFlow({
-    host: process.env.CTP_AUTH_URL,
-    projectKey,
-    credentials: {
-      clientId: process.env.CTP_CLIENT_ID,
-      clientSecret: process.env.CTP_CLIENT_SECRET,
-    },
-    fetch,
-  });
-
-  const httpMiddleware = createHttpClient({
-    host: process.env.CTP_API_URL,
-    fetch,
-  });
-
-  const client = createClient({
-    middlewares: [authMiddleware, httpMiddleware],
-  });
-
-  return client;
-}
-
-// Configure authMiddlewareOptions
+export const projectKey = import.meta.env.VITE_PROJECT_KEY || '';
 const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: `https://auth.${region}.commercetools.com`,
+  host: 'https://auth.europe-west1.gcp.commercetools.com',
   projectKey,
   credentials: {
-    clientId,
-    clientSecret,
+    // clientId: process.env.CTP_CLIENT_ID || '',
+    // clientSecret: process.env.CTP_CLIENT_SECRET || '',
+    clientId: import.meta.env.VITE_CLIENT_ID || '',
+    clientSecret: import.meta.env.VITE_CLIENT_SECRET || '',
   },
-  scopes,
+  scopes: [`manage_project:${projectKey}`],
   fetch,
 };
 
-// Configure httpMiddlewareOptions
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: `https://api.${region}.commercetools.com`,
+  host: 'https://api.europe-west1.gcp.commercetools.com',
   fetch,
 };
 
-// Export the ClientBuilder
-export const ctpClient = new ClientBuilder()
-  .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
+const client: Client = new ClientBuilder()
+  .withProjectKey(projectKey)
   .withClientCredentialsFlow(authMiddlewareOptions)
   .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware() // Include middleware for logging
+  .withLoggerMiddleware()
   .build();
+
+export const getApiRoot: () => ApiRoot = () => createApiBuilderFromCtpClient(client);
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'node-fetch': 'isomorphic-fetch',
+    },
+  },
 });

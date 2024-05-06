d138ecdfe522849bd93fa9fb8a65c0362ce33170
D4n1el13and37 <daniel837@mail.ru>
2024-05-06 22:54:21 +0300

    feat: add button to the ui component, fix: comment all api manipulation at App.tsx and render button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { useState, useEffect } from 'react';
-import { projectKey, getApiRoot } from './api/BuildClient';
+// import { useState, useEffect } from 'react';
+// import { projectKey, getApiRoot } from './api/BuildClient';
 import './App.css';
+import Button from './components/ui/button/Button';
 
 function App() {
-  const [projectDetails, setProjectDetails] = useState({});
+  // const [projectDetails, setProjectDetails] = useState({});
 
-  const getProducts = async () => {
-    try {
-      const project = await getApiRoot().withProjectKey({ projectKey }).products().get().execute();
+  // const getProducts = async () => {
+  //   try {
+  //     const project = await getApiRoot()
+  //       .withProjectKey({ projectKey })
+  //       .products()
+  //       .get()
+  //       .execute();
 
-      setProjectDetails(project.body);
-    } catch (e) {
-      if (e instanceof Error) {
-        throw new Error(e.message);
-      } else {
-        throw new Error('An unknown error occurred');
-      }
-    }
-  };
+  //     setProjectDetails(project.body);
+  //   } catch (e) {
+  //     if (e instanceof Error) {
+  //       throw new Error(e.message);
+  //     } else {
+  //       throw new Error('An unknown error occurred');
+  //     }
+  //   }
+  // };
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+  // function handler() {
+  //   console.log('click');
+  // }
 
   return (
     <>
-      <div>Project Details</div>
-      {JSON.stringify(projectDetails, undefined, 2)}
+      <Button
+        // onClick={handler}
+        type="submit"
+        isMain={true}
+        isDisabled={true}
+        isFilled={true}
+        // title="VISTOR"
+      >
+        Victor
+      </Button>
     </>
   );
 }

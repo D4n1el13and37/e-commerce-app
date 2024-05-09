ede1c46a7dcbfdd658acae43bac47f5dcbda6841
D4n1el13and37 <daniel837@mail.ru>
2024-05-09 12:38:06 +0300

    feat: create input component, add styles

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,22 +1,38 @@
-// import { useState } from 'react';
+import React, { ChangeEvent } from 'react';
 import classes from './Input.module.scss';
 
 interface InputInterface {
   type: 'text' | 'password' | 'email' | 'date' | 'number' | 'search' | 'tel';
-  onChange?: () => void;
+  fieldName: string;
+  onChange: (value: string) => void;
   value?: string;
   placehodler?: string;
-  required?: boolean;
-  fieldName?: string;
+  isRequired?: boolean;
+  width?: string;
+  error?: string;
 }
 
-function Input({ ...props }: InputInterface) {
+const Input: React.FC<InputInterface> = ({ ...props }) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    props.onChange(event.target.value);
+  };
+
   return (
-    <label className={`${classes}`}>
-      {props.fieldName && <span>{props.fieldName}</span>}
-      <input type={props.type} value={props.value} />
-    </label>
+    <div className={classes.container} style={{ width: props.width }}>
+      <label className={classes.label}>
+        {props.fieldName && <span>{props.fieldName}</span>}
+      </label>
+      <input
+        className={classes.input}
+        onChange={handleChange}
+        placeholder={props.placehodler}
+        type={props.type}
+        value={props.value}
+        required={props.isRequired}
+      />
+      {props.error && <p className={classes.error}>{props.error}</p>}
+    </div>
   );
-}
+};
 
 export default Input;

2bf9c8865da2e6c2780cdd1124bb5ad048c44401
D4n1el13and37 <daniel837@mail.ru>
2024-05-10 13:27:40 +0300

    fix: update component to FC, add media query

--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -3,13 +3,10 @@ import classes from './Checkbox.module.scss';
 
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  isDisabled?: boolean;
-  isChecked?: boolean;
-  isRequred?: boolean;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ ...props }) => {
-  const [isChecked, setIsChecked] = useState(!!props.isChecked);
+const Checkbox: React.FC<CheckboxProps> = ({ label, ...props }) => {
+  const [isChecked, setIsChecked] = useState(!!props.checked);
   function handleChange() {
     setIsChecked(!isChecked);
   }
@@ -24,12 +21,9 @@ const Checkbox: React.FC<CheckboxProps> = ({ ...props }) => {
           className={`${classes.input}`}
           type="checkbox"
           data-testid="checkbox"
-          checked={isChecked}
           onChange={handleChange}
-          required={!!props.isRequred}
-          disabled={!!props.isDisabled}
         ></input>
-        {props.label && <span>{props.label}</span>}
+        {label && <span>{label}</span>}
       </label>
     </>
   );

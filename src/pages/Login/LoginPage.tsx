9eef5f0bb2b51e2ecebbdd887e40f2359b869ccc
D4n1el13and37 <daniel837@mail.ru>
2024-05-12 20:02:28 +0300

    feat: add styles, update layout

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -30,45 +30,70 @@ function LoginPage() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <Input
-            type="text"
-            label="Email address"
-            id="email"
-            placeholder="user@example.com"
-            error={errors?.email?.message}
-            {...register('email', {
-              required: 'Email is requred',
-              validate: (value) => {
-                if (!value.includes('@')) {
-                  return 'Email shoud contain @';
-                }
-                if (!/^[^\s@]+/.test(value)) {
-                  return 'Email should contain a valid username';
-                }
-                if (/\s/.test(value)) {
-                  return 'Email should not contain spaces';
-                }
-                if (!/\w+\.\w+/.test(value)) {
-                  return 'Email should contain a valid domain name';
-                }
-                return true;
-              },
-            })}
+    <div className="login__container">
+      {/* <Button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="11"
+          height="19"
+          viewBox="0 0 11 19"
+          fill="none"
+        >
+          <path
+            d="M10.479 0.604004L2.02063 9.06238L10.479 17.5208"
+            stroke="#758963"
+            stroke-width="1.5"
           />
-          {errors.email && (
-            <span className="error">{errors.email.message}</span>
-          )}
+        </svg>
+      </Button> */}
+      <div className="headline">
+        <h2>Hello, Welcome back!</h2>
+        <span>Sign In to continue</span>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form_container">
+          <div className="input_container">
+            <Input
+              type="text"
+              label="Email address"
+              id="email"
+              placeholder="user@example.com"
+              error={errors?.email?.message}
+              {...register('email', {
+                required: 'Email is requred',
+                validate: (value) => {
+                  if (!value.includes('@')) {
+                    return 'Email should contain @';
+                  }
+                  if (!/^[^\s@]+/.test(value)) {
+                    return 'Should contain a valid username';
+                  }
+                  if (/\s/.test(value)) {
+                    return 'Email should not contain spaces';
+                  }
+                  if (!/\w+\.\w+/.test(value)) {
+                    return 'Should contain a valid domain name';
+                  }
+                  return true;
+                },
+              })}
+            />
+            <div className="error_container">
+              {errors.email && (
+                <span className="error">{errors.email.message}</span>
+              )}
+            </div>
+          </div>
+          <div className="input_container">
+            <PasswordField register={register} errors={errors} />
+            <div className="error_container">
+              <span className="error">
+                {errors.password && errors.password.message}
+              </span>
+            </div>
+          </div>
         </div>
-        <div>
-          <PasswordField register={register} errors={errors} />
-          <span className="error">
-            {errors.password && errors.password.message}
-          </span>
-        </div>
-        <Button disabled={isSubmitting} isMain={true}>
+        <Button isFilled={true} disabled={isSubmitting} isMain={true}>
           {isSubmitting ? 'Loading...' : 'Submit'}
         </Button>
       </form>

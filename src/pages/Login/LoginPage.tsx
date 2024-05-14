9f0ed9078826c5da0b0537d8c7dcc9a06326efbc
D4n1el13and37 <daniel837@mail.ru>
2024-05-14 17:31:38 +0300

    feat: add email component

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,9 +1,8 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
-import Input from '../../components/ui/input/Input';
 import Button from '../../components/ui/button/Button';
-import PasswordField from '../../components/form/PasswordInput';
+import PasswordField from '../../components/form/password/PasswordInput';
 import './LoginPage.scss';
-// import Checkbox from './components/ui/checkbox/Checkbox';
+import EmailInput from '../../components/form/email/EmailInput';
 
 export interface LoginForm {
   email: string;
@@ -53,31 +52,7 @@ function LoginPage() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form_container">
           <div className="input_container">
-            <Input
-              type="text"
-              label="Email address"
-              id="email"
-              placeholder="user@example.com"
-              error={errors?.email?.message}
-              {...register('email', {
-                required: 'Email is requred',
-                validate: (value) => {
-                  if (!value.includes('@')) {
-                    return 'Email should contain @';
-                  }
-                  if (!/^[^\s@]+/.test(value)) {
-                    return 'Should contain a valid username';
-                  }
-                  if (/\s/.test(value)) {
-                    return 'Email should not contain spaces';
-                  }
-                  if (!/\w+\.\w+/.test(value)) {
-                    return 'Should contain a valid domain name';
-                  }
-                  return true;
-                },
-              })}
-            />
+            <EmailInput register={register} errors={errors} />
             <div className="error_container">
               {errors.email && (
                 <span className="error">{errors.email.message}</span>

4dfc5e0091fe1ed08d454fe636821869d29188e8
D4n1el13and37 <daniel837@mail.ru>
2024-05-14 17:32:46 +0300

    refactor: add comments to client builder and index.scss

--- src/api/BuildClient.ts
+++ src/api/BuildClient.ts
@@ -11,22 +11,23 @@ import {
 
 export const projectKey = import.meta.env.VITE_PROJECT_KEY || '';
 const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: 'https://auth.europe-west1.gcp.commercetools.com',
-  projectKey,
+  host: 'https://auth.europe-west1.gcp.commercetools.com', // where we send auth
+  projectKey, // it's from .env file
   credentials: {
     clientId: import.meta.env.VITE_CLIENT_ID || '',
     clientSecret: import.meta.env.VITE_CLIENT_SECRET || '',
   },
-  scopes: [`manage_project:${projectKey}`],
+  scopes: [`manage_project:${projectKey}`], // here we choose which permission we want to get
   fetch,
 };
 
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  // where we send auth - if it is the same mb we create an .env host name?
   host: 'https://api.europe-west1.gcp.commercetools.com',
   fetch,
 };
 
-const client: Client = new ClientBuilder()
+const client: Client = new ClientBuilder() // as i understande here we crreate a client
   .withProjectKey(projectKey)
   .withClientCredentialsFlow(authMiddlewareOptions)
   .withHttpMiddleware(httpMiddlewareOptions)

81999c7c9e894dc32397c34a65418faa52dcefaf
Mineclinee <vasily-minecline@yandex.ru>
2024-05-17 01:58:57 +0300

    feat: add navigate to login page in header

--- src/components/header/headerAuthBtns/HeaderAuthBtns.tsx
+++ src/components/header/headerAuthBtns/HeaderAuthBtns.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../ui/button/Button';
 import classes from '../Header.module.scss';
 
-const HeaderAuthBtns: React.FC = () => (
-  <ul className={classes.shop_nav__list}>
-    <li>
-      <Button type="submit" isMain={true}>
-        Log In
-      </Button>
-    </li>
-    <li>
-      <Button type="submit" isMain={true} isFilled={true}>
-        Sig In
-      </Button>
-    </li>
-  </ul>
-);
+const HeaderAuthBtns: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('login');
+  };
+
+  return (
+    <ul className={classes.shop_nav__list}>
+      <li>
+        <Button type="submit" isMain={true} onClick={handleLoginClick}>
+          Log In
+        </Button>
+      </li>
+      <li>
+        <Button type="submit" isMain={true} isFilled={true}>
+          Sig In
+        </Button>
+      </li>
+    </ul>
+  );
+};
 
 export default HeaderAuthBtns;

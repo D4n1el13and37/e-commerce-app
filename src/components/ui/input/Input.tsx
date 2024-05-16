5ec6c0f87f29bdefaca329e8022156fee9dbdb62
D4n1el <87085006+D4n1el13and37@users.noreply.github.com>
2024-05-16 17:50:51 +0300

    Feat: Login Form RSS_ECOMM_2_01 (#17)
    * fix: edit printWidth property for prettier option, from 120 to 80
    
    * fix: comment all default vite styles
    
    * feat: add button to the ui component, fix: comment all api manipulation at App.tsx and render button component
    
    * feat: add different types of buttons at page
    
    * fix: styles for different types of button
    
    * refactor: change extantion to scss, rename variables, change disabled color button
    
    * fix: fix space between class names
    
    * feat: create two simple tests for button
    
    * feat: add jsdom packages add tsconfig types, fix: edit path to test setup
    
    * fix: remove tesing id from button
    
    * fix: fix classname output (remove spaces if button with only one class), add data-testid attribute
    
    * feat: covered the button with tests
    
    * feat: create checkbox component, add styles
    
    * feat: add tests for checkbox component
    
    * feat: add testing id for label
    
    * refactor: remove unnecessary class, add classname prop to input
    
    * feat: add disabled styles
    
    * feat: add two more tests for toggle state and disabled checkbox
    
    * feat: add checkboxes to the page
    
    * feat: add input component
    
    * feat: create input component, add styles
    
    * refactor: add Input component into app(and comment them to avoid lint troubles)
    
    * feat: add transition for background when cheked
    
    * fix: update Button element
    
    * feat: add prefers-reduced-motion media
    
    * fix: rework to functional component
    
    * fix: update component to FC, add media query
    
    * fix: made it Function Component
    
    * feat: add Input component
    
    * fix: fix checkbox
    
    * feat: add tests for input, update checkboxes tests
    
    * feat: add Input component at page
    
    * feat: update path to modules at index.scss
    
    * refactor: remove debug at input test
    
    * feat: add error class, fix focus border radius and focun on input
    
    * feat: add Input Password component
    
    * refactor: update error state and fields names
    
    * feat: create login page, implement login form
    
    * feat: add login page at App component, comment all other components
    
    * feat: add styles, update layout
    
    * fix: add error styles into input component
    
    * refactor: devide into small components, feat: add password validation helper, add toggle button component, update password component into custom input
    
    * feat: create email component
    
    * feat: add email component
    
    * refactor: add comments to client builder and index.scss
    
    * fix: remove unnecessary test(bcs remove width attribute)
    
    * fix: add autocompleate, revork validation to password and email components
    
    * refactor: update styles lint
    
    * fix: headline

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,37 +1,29 @@
-import React, { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, forwardRef } from 'react';
 import classes from './Input.module.scss';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type: 'text' | 'password' | 'email' | 'date' | 'number' | 'search' | 'tel';
-  fieldName: string;
+  label: string;
   id: string;
-  width?: string;
   error?: string;
 }
 
-const Input: React.FC<InputProps> = ({
-  type,
-  id,
-  fieldName,
-  width,
-  ...props
-}) => (
-  <div
-    className={classes.container}
-    style={{ width }}
-    data-testid="input-container"
-  >
-    <label htmlFor={id} className={classes.label}>
-      {fieldName && <span>{fieldName}</span>}
-    </label>
-    <input
-      data-testid="input"
-      id={id}
-      type={type}
-      className={classes.input}
-      {...props}
-    />
-  </div>
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type, id, label, error, ...props }, ref) => (
+    <div className={classes.container} data-testid="input-container">
+      <label htmlFor={id} className={classes.label}>
+        {label && <span>{label}</span>}
+      </label>
+      <input
+        data-testid="input"
+        ref={ref}
+        id={id}
+        type={type}
+        className={`${classes.input} ${error && classes.error}`}
+        {...props}
+      />
+    </div>
+  )
 );
 
 export default Input;

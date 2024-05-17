ece4325fb82301ac0622c4aab83879b7f23599f0
Vasily <52579648+Mineclinee@users.noreply.github.com>
2024-05-17 16:13:17 +0300

    Feat: routing implementation (#22)
    * feat: add basic router
    
    * feat: add return to previous page from 404 page
    
    * feat: add routin to header
    
    * feat: add navigate to login page in header
    
    * refator: remove comments from app.tsx and add basic styles to login page
    
    * refator: remove unnecessary comments from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import Register from './pages/Register/Register';
-// import LoginPage from './pages/Login/LoginPage';
-// import Home from './pages/Home/Home';
-// import NotFound from './pages/NotFound/NotFound';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 import LoginPage from './pages/Login/LoginPage';
 
 function App() {
@@ -28,36 +27,11 @@ function App() {
   // }, []);
 
   return (
-    // <div style={{ display: 'flex', flexWrap: 'wrap', gap: '8px' }}>
-    //   <Checkbox label="Are you sure?" isChecked={true} />
-    //   <Checkbox isRequred={true} />
-    //   <Checkbox isDisabled={true} />
-    //   <Button type="submit" isMain={true} isFilled={true}>
-    //     Filled
-    //   </Button>
-    //   <Button isMain={true} type="submit">
-    //     Main
-    //   </Button>
-    //   <Button>+</Button>
-    //   <Button isFilled={true} isDisabled={true}>
-    //     -
-    //   </Button>
-    // </div>
-    <>
-      <div
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          gap: '8px',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100dvh',
-        }}
-      >
-        <LoginPage />
-      </div>
-      <Register />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="login" element={<LoginPage />} />
+    </Routes>
   );
 }
 

d789d2174744bbcbc9c7cf04226b4143edecb837
D4n1el13and37 <daniel837@mail.ru>
2024-05-18 23:29:23 +0300

    feat: add redux store types(for TS), add methods to slicer and rework for auth

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,35 +1,92 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Customer } from '@commercetools/platform-sdk';
+import { loginByToken, loginWithPassword } from '../api/authMethods';
+import { LoginForm } from '../pages/Login/LoginPage';
 
-const key = 'tokendata';
-const storedData = localStorage.getItem(key);
-const { token } = storedData ? JSON.parse(storedData) : { token: null };
+// const key = 'tokendata';
+// const storedData = localStorage.getItem(key);
+// const { token } = storedData ? JSON.parse(storedData) : { token: null };
 
-const initialState = {
-  token,
-  isAuthenticated: !!token,
+export interface AutorizationState {
+  currentUser: null | Customer;
+  isAutorized: boolean;
+  isLoading: boolean;
+}
+
+const initialState: AutorizationState = {
+  currentUser: null,
+  isAutorized: false,
+  isLoading: false,
 };
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (userData: LoginForm, thunkAPI) => {
+    try {
+      const { email, password } = userData;
+      const response = await loginWithPassword(email, password);
+      // console.warn(response);
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message); // ???
+      }
+      throw new Error('Error from REDUX login function');
+    }
+  }
+);
+
+export const autorizationByToken = createAsyncThunk(
+  'auth/loginByToken',
+  async (_, thunkAPI) => {
+    try {
+      const response = await loginByToken();
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message); // ???
+      }
+      throw new Error('Error from REDUX login function');
+    }
+  }
+);
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    login: (_state, action: PayloadAction<{ token: string }>) => {
-      const newState = {
-        token: action.payload.token,
-        isAuthenticated: true,
-      };
-      // console.log('User log-in from REDUX');
-      return newState;
-    },
-    logout: () => {
-      localStorage.removeItem(key);
-      return {
-        token: null,
-        isAuthenticated: false,
-      };
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        const newState = state;
+        newState.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        const newState = state;
+        newState.isLoading = false;
+        newState.isAutorized = true;
+        newState.currentUser = action.payload;
+      })
+      .addCase(login.rejected, (state) => {
+        const newState = state;
+        newState.isLoading = false;
+      })
+      .addCase(autorizationByToken.pending, (state) => {
+        const newState = state;
+        newState.isLoading = true;
+      })
+      .addCase(autorizationByToken.fulfilled, (state, action) => {
+        const newState = state;
+        newState.isLoading = false;
+        newState.isAutorized = true; // do we actually need it?
+        newState.currentUser = action.payload;
+      })
+      .addCase(autorizationByToken.rejected, (state) => {
+        const newState = state;
+        newState.isLoading = false;
+        newState.currentUser = null;
+      });
   },
 });
 
-export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -7,4 +7,7 @@ const store = configureStore({
   },
 });
 
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
 export default store;

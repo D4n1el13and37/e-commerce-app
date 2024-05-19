7c935e0abbc7c04cb7666b7513451c53039d597d
D4n1el13and37 <daniel837@mail.ru>
2024-05-19 19:52:37 +0300

    feat: add area-label for go back button, refactor: login page styles

--- src/components/form/goBack/GoBackButton.tsx
+++ src/components/form/goBack/GoBackButton.tsx
@@ -10,7 +10,7 @@ const GoBackButton: React.FC = () => {
   };
 
   return (
-    <Button onClick={handleBack}>
+    <Button aria-label={`Go back`} onClick={handleBack}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="11"

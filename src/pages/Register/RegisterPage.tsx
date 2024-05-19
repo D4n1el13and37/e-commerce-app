ac62583f4942aa11881be73d8e2ab3f365100fdf
D4n1el13and37 <daniel837@mail.ru>
2024-05-19 21:48:27 +0300

    feat: change back button into component goBack button

--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -1,27 +1,12 @@
 import RegisterForm from './Component/RegisterForm';
-import Button from '../../components/ui/button/Button';
 import classes from './Component/Rigister.module.scss';
+import GoBackButton from '../../components/form/goBack/GoBackButton';
 
 export default function RegisterPage() {
   return (
     <section className={`container`}>
       <div className={`grid ${classes.register}`}>
-        <Button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="12"
-            height="19"
-            viewBox="0 0 12 19"
-            fill="none"
-            aria-label="Go Back"
-          >
-            <path
-              d="M10.5 18L2 9.5L10.5 1"
-              stroke="#758963"
-              strokeWidth="1.5"
-            />
-          </svg>
-        </Button>
+        <GoBackButton />
         <RegisterForm />
       </div>
     </section>

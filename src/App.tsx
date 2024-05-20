5c92b793a4915ae8f16ac12480c27985304f77ed
D4n1el13and37 <daniel837@mail.ru>
2024-05-20 11:29:33 +0300

    fix: create browser router, handle wrong path, add path to not autorized users

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 import { useEffect } from 'react';
 import Register from './pages/Register/Register';
 import LoginPage from './pages/Login/LoginPage';
@@ -7,6 +11,7 @@ import { autorizationByToken } from './store/authSlice';
 import useAppDispatch from './hooks/useAppDispatch';
 import useAppSelector from './hooks/useAppSelector';
 import NotFound from './pages/NotFound/NotFound';
+import NeedAutorizePage from './pages/NeedAutorizePage/NeedAutorizePage';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -20,36 +25,32 @@ function App() {
   // через маунт эффект вызвать чек авторизэйшн (ждем и показываем крутилку)
   // const [autorized, setAutorized] = useState(false);
 
-  return (
-    <Routes>
-      <Route path="/" errorElement={<NotFound />} element={<Home />} />
-      <Route
-        path="/login"
-        errorElement={<NotFound />}
-        element={!isAuthorized ? <LoginPage /> : <Navigate to="/main" />}
-      />
-      <Route
-        path="/register"
-        errorElement={<NotFound />}
-        element={!isAuthorized ? <Register /> : <Navigate to="/main" />}
-      />
-      {/* <Route path="/home" errorElement={<NotFound />} element={<Home />} /> */}
-      <Route
-        path="/main"
-        errorElement={<NotFound />}
-        element={isAuthorized ? <Home /> : <Navigate to="/login" />}
-      />
-      {/* <Route path="/*" element={<LoginPage />} /> */}
-      {/* <Route
-    //   path="/protected"
-    //   element={
-    //     <RequireAuth>
-    //       {/* <ProtectedPage />
-          </RequireAuth>
-        }
-      /> */}
-    </Routes>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+        {
+          path: 'login',
+          element: !isAuthorized ? <LoginPage /> : <Navigate to="/main" />,
+        },
+        {
+          path: 'register',
+          element: !isAuthorized ? <Register /> : <Navigate to="/main" />,
+        },
+        {
+          path: 'main',
+          element: isAuthorized ? <Home /> : <NeedAutorizePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;

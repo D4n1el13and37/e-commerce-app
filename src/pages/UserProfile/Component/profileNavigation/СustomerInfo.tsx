bf751b9b97610a641a4a60dc8503fef587642601
Anna <cainowa@gmail.com>
2024-05-26 20:05:25 +0200

    feat: add validation for customer

--- src/pages/UserProfile/Component/profileNavigation/СustomerInfo.tsx
+++ src/pages/UserProfile/Component/profileNavigation/СustomerInfo.tsx
@@ -1,35 +1,88 @@
 import cn from 'classnames';
 
-import React from 'react';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import Input from '../../../../components/ui/input/Input';
 import { UserProps } from '../UserInfo';
 
 import classes from '../../userProfile.module.scss';
+import Button from '../../../../components/ui/button/Button';
 
-const CustomerInfo: React.FC<UserProps> = ({ dataUser }) => (
-  <div className={cn(classes.profileData__data)}>
-    <div className={cn(classes.profileData__field)}>
-      <Input
-        id="firstName"
-        autoComplete="firstName"
-        label="First Name"
-        type="text"
-        value={dataUser?.firstName ?? ''}
-      />
-      <Input
-        id="lastName"
-        label="Last Name"
-        type="text"
-        value={dataUser?.lastName ?? ''}
-      />
-      <Input
-        id="dateBirth"
-        label="Date of Birth"
-        type="date"
-        value={dataUser?.dateOfBirth ?? ''}
-      />
+interface CustomerInfoData {
+  firstName: string;
+  lastName: string;
+  dateBirth: string;
+}
+
+const CustomerInfo: React.FC<UserProps> = ({ dataUser }) => {
+  const methods = useForm<CustomerInfoData>({
+    mode: 'onChange',
+    defaultValues: {
+      firstName: dataUser?.firstName || '',
+      lastName: dataUser?.lastName || '',
+      dateBirth: dataUser?.dateOfBirth || '',
+    },
+  });
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = methods;
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const onSubmit: SubmitHandler<CustomerInfoData> = async (data) => {
+    console.log(data);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className={classes.profileData__data}>
+      <form
+        className={classes.profileData__field}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className={classes.input_container}>
+          <Input
+            id="firstName"
+            autoComplete="firstName"
+            label="First Name"
+            type="text"
+            {...register('firstName', {
+              required: 'Required',
+              minLength: {
+                value: 1,
+                message: 'At least 1 character',
+              },
+              validate: (value: string) => {
+                const regexFirstName = /^[a-zA-Z]+$/;
+                if (!regexFirstName.test(value)) {
+                  return 'Only latin letters';
+                }
+                return true;
+              },
+            })}
+          />
+          <div className={`${classes.error_container}`}>
+            {errors.firstName && (
+              <span className="error">
+                {errors.firstName.message as string}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className={classes.input_container}>
+          <Input id="lastName" label="Last Name" type="text" />
+        </div>
+        <Input id="dateBirth" label="Date of Birth" type="date" />
+      </form>
+      <Button isFilled={true} isMain={true}>
+        {' '}
+        onS Save Change
+      </Button>
     </div>
-  </div>
-);
+  );
+};
 
 export default CustomerInfo;

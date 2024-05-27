5f105eef357a50d0e39e2153b81a12eecc89c3f2
annkainova <cainowa@gmail.com>
2024-05-27 11:46:05 +0200

    Feat: display personal info in account page (#54)
    * feat: add account button and attach navigate to account
    
    * feat: add block user info
    
    * feat: add navigation part in user profile page
    
    * feat: displayed user information
    
    * feat: add display default address
    
    * fix: fix type user
    
    * fix: apply eslint
    
    * fix: fix semantics, delete unnecessary code

---
+++ src/pages/UserProfile/Component/profileNavigation/СustomerInfo.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Input from '../../../../components/ui/input/Input';
+import { UserProps } from '../UserInfo';
+
+import classes from '../../userProfile.module.scss';
+
+const CustomerInfo: React.FC<UserProps> = ({ dataUser }) => (
+  <div className={classes.profileData__data}>
+    <div className={classes.profileData__field}>
+      <Input
+        id="firstName"
+        autoComplete="firstName"
+        label="First Name"
+        type="text"
+        value={dataUser?.firstName ?? ''}
+      />
+      <Input
+        id="lastName"
+        label="Last Name"
+        type="text"
+        value={dataUser?.lastName ?? ''}
+      />
+      <Input
+        id="dateBirth"
+        label="Date of Birth"
+        type="date"
+        value={dataUser?.dateOfBirth ?? ''}
+      />
+    </div>
+  </div>
+);
+
+export default CustomerInfo;
---
+++ src/pages/UserProfile/UserProfile.tsx
@@ -0,0 +1,43 @@
+import cn from 'classnames';
+
+import React, { useEffect, useState } from 'react';
+import { Customer } from '@commercetools/platform-sdk';
+import UserInfo from './Component/UserInfo';
+import ProfileInfo from './Component/ProfileInfo';
+import Header from '../../components/header/Header';
+
+import classes from './userProfile.module.scss';
+import getCustomer from '../../api/Customer/customer';
+
+const UserProfile: React.FC = () => {
+  const [user, setUser] = useState<Customer | undefined>();
+
+  useEffect(() => {
+    const fetchCustomer = async () => {
+      try {
+        const customer = await getCustomer();
+        setUser(customer);
+      } catch (e) {
+        if (e instanceof Error) {
+          throw new Error(e.message);
+        }
+      }
+    };
+
+    fetchCustomer();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className={cn('container')}>
+        <div className={cn('grid', classes.account)}>
+          <UserInfo dataUser={user} />
+          <ProfileInfo dataUser={user} />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default UserProfile;

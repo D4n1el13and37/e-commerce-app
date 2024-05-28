ab5730300575b5f1ae961ef844ce2424709b336b
D4n1el13and37 <daniel837@mail.ru>
2024-05-28 17:47:49 +0300

    feat: add categories into state, add fetch products by category method, fix: fetch products method

--- src/store/productsSlice.ts
+++ src/store/productsSlice.ts
@@ -1,14 +1,29 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { Product } from '@commercetools/platform-sdk';
-import { getProducts } from '../api/products/productsMethods';
+import { Category, Image, LocalizedString } from '@commercetools/platform-sdk';
+import {
+  getCardsByCategory,
+  getCategories,
+  getProducts,
+} from '../api/products/productsMethods';
+
+interface CustomProduct {
+  title: LocalizedString;
+  description: LocalizedString;
+  price: number;
+  salePrice: number;
+  images: Image[] | undefined;
+  id?: string;
+}
 
 export interface ProductState {
-  productsList: Product[];
+  productsList: CustomProduct[];
+  categoriesList: Category[];
   isLoading: boolean;
 }
 
 const initialState: ProductState = {
   productsList: [],
+  categoriesList: [],
   isLoading: false,
 };
 
@@ -17,12 +32,69 @@ export const fetchProducts = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await getProducts();
-      return response;
+      const answer: CustomProduct[] = [];
+      response.results.forEach((card) => {
+        const data = {
+          title: card.masterData.current.name,
+          description: card.masterData.current.description!,
+          price:
+            card.masterData.current.masterVariant.prices![0].value.centAmount,
+          salePrice:
+            card.masterData.current.masterVariant.prices![0].discounted!.value
+              .centAmount,
+          id: card.id,
+          images: card.masterData.current.masterVariant.images,
+        };
+        answer.push(data);
+      });
+      return answer;
     } catch (error) {
       if (error instanceof Error) {
         return thunkAPI.rejectWithValue(error.message); // ???
       }
       throw new Error('Error from REDUX login function');
+    }
+  }
+);
+
+export const fetchCategories = createAsyncThunk(
+  'products/categories',
+  async (_, thunkAPI) => {
+    try {
+      const response = await getCategories();
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      throw new Error('Error fetching categories');
+    }
+  }
+);
+
+export const fetchProductsByCategory = createAsyncThunk(
+  'products/productsByCategory',
+  async (categoryId: string, thunkAPI) => {
+    try {
+      const response = await getCardsByCategory(categoryId);
+      const answer: CustomProduct[] = [];
+      response.results.forEach((card) => {
+        const data = {
+          title: card.name,
+          description: card.description!,
+          price: card.masterVariant.prices![0].value.centAmount,
+          salePrice: card.masterVariant.prices![0].discounted!.value.centAmount,
+          id: card.id,
+          images: card.masterVariant.images,
+        };
+        answer.push(data);
+      });
+      return answer;
+    } catch (error) {
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      throw new Error('Error fetching products by category');
     }
   }
 );
@@ -40,9 +112,35 @@ const productsSlice = createSlice({
       .addCase(fetchProducts.fulfilled, (state, action) => {
         const newState = state;
         newState.isLoading = false;
-        newState.productsList = action.payload.results;
+        newState.productsList = action.payload;
       })
       .addCase(fetchProducts.rejected, (state) => {
+        const newState = state;
+        newState.isLoading = false;
+      })
+      .addCase(fetchCategories.pending, (state) => {
+        const newState = state;
+        newState.isLoading = true;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        const newState = state;
+        newState.isLoading = false;
+        newState.categoriesList = action.payload.results;
+      })
+      .addCase(fetchCategories.rejected, (state) => {
+        const newState = state;
+        newState.isLoading = false;
+      })
+      .addCase(fetchProductsByCategory.pending, (state) => {
+        const newState = state;
+        newState.isLoading = true;
+      })
+      .addCase(fetchProductsByCategory.fulfilled, (state, action) => {
+        const newState = state;
+        newState.isLoading = false;
+        newState.productsList = action.payload;
+      })
+      .addCase(fetchProductsByCategory.rejected, (state) => {
         const newState = state;
         newState.isLoading = false;
       });

176d47f6352c36c9d8ed5bebff211a7ec44be6f5
D4n1el13and37 <daniel837@mail.ru>
2024-06-01 17:20:02 +0300

    feat: add crumbs into catalog page, refactor output of product list, update fetch depends on cat/subcat

--- src/pages/Catalog/CatalogPage.tsx
+++ src/pages/Catalog/CatalogPage.tsx
@@ -1,33 +1,55 @@
 import React, { useEffect } from 'react';
+import { Outlet, useParams } from 'react-router-dom';
 import cn from 'classnames';
+import cl from './CatalogPage.module.scss';
 import Header from '../../components/header/Header';
 import useAppDispatch from '../../hooks/useAppDispatch';
-import { RootState } from '../../store/store';
-import { fetchProducts, fetchCategories } from '../../store/productsSlice';
-import useAppSelector from '../../hooks/useAppSelector';
+import {
+  fetchProducts,
+  fetchCategories,
+  fetchProductsByCategory,
+} from '../../store/productsSlice';
 import CategoriesList from './components/categories/CategoriesList';
-import ProductList from './components/product_list/ProductList';
+// import ProductList from './components/product_list/ProductList';
 import Footer from '../../components/footer/Footer';
-
-import cl from './CatalogPage.module.scss';
+import Breadcrumbs from '../../components/ui/crumbs/BreadCrumbs';
+import useAppSelector from '../../hooks/useAppSelector';
 
 const CatalogPage: React.FC = () => {
   const dispatch = useAppDispatch();
-  const { productsList, language } = useAppSelector(
-    (state: RootState) => state.products
-  );
+  const { categoryName, subcategoryName } = useParams<{
+    categoryName: string;
+    subcategoryName?: string;
+  }>();
+  const { categoriesList } = useAppSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(fetchProducts());
-    dispatch(fetchCategories());
-  }, [dispatch]);
+    // load category list if we don't have it on state
+    if (categoriesList.length === 0) {
+      dispatch(fetchCategories());
+    }
+
+    // Choose category for upload products
+    const categoryToLoad = subcategoryName || categoryName;
+    if (categoryToLoad) {
+      const category = categoriesList.find(
+        (cat) => cat.name === categoryToLoad
+      );
+      if (category) {
+        dispatch(fetchProductsByCategory(category.id));
+      }
+    } else {
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, categoryName, subcategoryName, categoriesList]);
 
   return (
     <>
       <Header />
       <main className={cn(cl.catalog__wrapper, 'container', 'grid')}>
+        <Breadcrumbs />
         <CategoriesList />
-        <ProductList products={productsList} language={language} />
+        <Outlet />
       </main>
       <Footer />
     </>

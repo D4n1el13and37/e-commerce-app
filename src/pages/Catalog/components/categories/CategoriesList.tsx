933b02a12c5d85c1dd1d6c46682e772e5f53e46a
D4n1el13and37 <daniel837@mail.ru>
2024-06-03 16:02:36 +0300

    feat: add adaprive at page

--- src/pages/Catalog/components/categories/CategoriesList.tsx
+++ src/pages/Catalog/components/categories/CategoriesList.tsx
@@ -5,6 +5,7 @@ import cl from './Categories.module.scss';
 import useAppDispatch from '../../../../hooks/useAppDispatch';
 import useAppSelector from '../../../../hooks/useAppSelector';
 import { fetchProductsByCategory } from '../../../../store/productsSlice';
+import Button from '../../../../components/ui/button/Button';
 
 const CategoriesList: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,6 +18,7 @@ const CategoriesList: React.FC = () => {
   }>();
 
   const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     if (subcategoryName) {
@@ -37,6 +39,9 @@ const CategoriesList: React.FC = () => {
   }) => {
     dispatch(fetchProductsByCategory(categoryID));
     setActiveCategory(category);
+    if (modalOpen) {
+      setModalOpen(false);
+    }
     if (parentCategoryName) {
       navigate(`/catalog/${parentCategoryName}/${category}`);
     } else {
@@ -82,7 +87,30 @@ const CategoriesList: React.FC = () => {
         </div>
       ));
 
-  return <div className={cl.wrapper}>{renderCategories()}</div>;
+  const toggleMenuOpen = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  return (
+    <>
+      <div className={cl.button__wrapper}>
+        <Button isMain={true} onClick={toggleMenuOpen}>
+          Show Categories
+        </Button>
+      </div>
+      <div className={cl.wrapper}>{renderCategories()}</div>
+      {modalOpen && (
+        <div className={cn(cl.modal, cl.burger, { [cl.active]: modalOpen })}>
+          <div className={cl.modal_content}>
+            <Button isMain={true} onClick={toggleMenuOpen}>
+              Back
+            </Button>
+            {renderCategories()}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CategoriesList;

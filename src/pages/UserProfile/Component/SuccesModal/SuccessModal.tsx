88be85878d2bf11e181a807252cf24a84ac0e60c
Anna <cainowa@gmail.com>
2024-06-03 19:12:41 +0200

    fix: make correction

--- src/pages/UserProfile/Component/SuccesModal/SuccessModal.tsx
+++ src/pages/UserProfile/Component/SuccesModal/SuccessModal.tsx
@@ -6,17 +6,12 @@ interface CustomModalProps {
   isOpen: boolean;
 }
 
-const SuccessModal: React.FC<CustomModalProps> = ({ isOpen }) => {
-  if (!isOpen) return null;
-
-  return (
-    <div className={`${modal.modal} ${isOpen ? modal.show : ''}`}>
-      <div className={modal.modal__content}>
-        <p className={modal.modal__text}>Success!</p>
-      </div>
-      <img src={srcPlants} alt="Illustartion happy plant" />
+const SuccessModal: React.FC<CustomModalProps> = ({ isOpen }) => (
+  <div className={`${modal.modal} ${isOpen ? modal.show : ''}`}>
+    <div className={modal.modal__content}>
+      <p className={modal.modal__text}>Success!</p>
     </div>
-  );
-};
-
+    <img src={srcPlants} alt="Illustartion happy plant" />
+  </div>
+);
 export default SuccessModal;

520f1501478f634d9b31280e71949090c9652a69
Mineclinee <vasily-minecline@yandex.ru>
2024-06-03 23:22:00 +0300

    feat: add adaptive layout to catalog page

--- src/pages/Catalog/components/categories/CategoriesList.tsx
+++ src/pages/Catalog/components/categories/CategoriesList.tsx
@@ -5,7 +5,6 @@ import cl from './Categories.module.scss';
 import useAppDispatch from '../../../../hooks/useAppDispatch';
 import useAppSelector from '../../../../hooks/useAppSelector';
 import { fetchProductsByCategory } from '../../../../store/productsSlice';
-import Button from '../../../../components/ui/button/Button';
 
 const CategoriesList: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -18,7 +17,6 @@ const CategoriesList: React.FC = () => {
   }>();
 
   const [activeCategory, setActiveCategory] = useState<string | null>(null);
-  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     if (subcategoryName) {
@@ -39,9 +37,6 @@ const CategoriesList: React.FC = () => {
   }) => {
     dispatch(fetchProductsByCategory(categoryID));
     setActiveCategory(category);
-    if (modalOpen) {
-      setModalOpen(false);
-    }
     if (parentCategoryName) {
       navigate(`/catalog/${parentCategoryName}/${category}`);
     } else {
@@ -87,30 +82,7 @@ const CategoriesList: React.FC = () => {
         </div>
       ));
 
-  // const toggleMenuOpen = () => {
-  //   setModalOpen(!modalOpen);
-  // };
-
-  return (
-    <>
-      <div className={cl.button__wrapper}>
-        <Button isMain={true} /* onClick={toggleMenuOpen} */>
-          Show Categories
-        </Button>
-      </div>
-      <div className={cl.wrapper}>{renderCategories()}</div>
-      {/* {modalOpen && (
-        <div className={cn(cl.modal, cl.burger, { [cl.active]: modalOpen })}>
-          <div className={cl.modal_content}>
-            <Button isMain={true} onClick={toggleMenuOpen}>
-              Back
-            </Button>
-            {renderCategories()}
-          </div>
-        </div>
-      )} */}
-    </>
-  );
+  return <div className={cl.wrapper}>{renderCategories()}</div>;
 };
 
 export default CategoriesList;

b3f67a3729d8103cbaec055b81b344cfc8dcbae1
D4n1el13and37 <daniel837@mail.ru>
2024-06-08 21:58:54 +0300

    feat: add empty cart component, implement styles at catalog page

--- src/pages/Basket/BasketPage.tsx
+++ src/pages/Basket/BasketPage.tsx
@@ -1,18 +1,34 @@
-// import React from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import BasketCard from './components/BasketCard';
 
-const BasketPage = () => (
-  <>
-    <Header />
-    <main>
-      <BasketCard />
-      <BasketCard />
-      <BasketCard />
-    </main>
-    <Footer />
-  </>
-);
+import cl from './BasketPage.module.scss';
+import EmptyCart from './components/empty/EmptyCart';
+
+const BasketPage = () => {
+  const mockArray = [];
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <div className={cl.main__wrapper}>
+          <h1 className={cl.main__title}>Shopping Bag</h1>
+
+          {/* mock checker if cart is empty */}
+          {mockArray.length ? (
+            <>
+              <BasketCard />
+              <BasketCard />
+              <BasketCard />
+            </>
+          ) : (
+            <EmptyCart />
+          )}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default BasketPage;

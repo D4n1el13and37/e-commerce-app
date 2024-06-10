65319c6b16f7e08000f9f390058b4bce815d964e
D4n1el13and37 <daniel837@mail.ru>
2024-06-10 18:00:07 +0300

    feat: add clear modal component

---
+++ src/pages/Basket/components/clear/ClearModal.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '../../../../components/ui/button/Button';
+import sureImage from './sure.svg';
+
+import cl from './ClearModal.module.scss';
+
+interface ClearModalProps {
+  cartClearHandle: () => void;
+  closeModal: () => void;
+}
+
+const ClearModal: React.FC<ClearModalProps> = ({
+  cartClearHandle,
+  closeModal,
+}) => {
+  const clearBasket = () => {
+    cartClearHandle();
+    closeModal();
+  };
+
+  return (
+    <div className={cl.modal__wrapper}>
+      <div className={cl.modal__content}>
+        <div className={cl.image__wrapper}>
+          <img src={sureImage} alt="" />
+        </div>
+        <div className={cl.text__wrapper}>
+          <h4 className={cl.text__title}>
+            Are you sure ? <br /> you want to clear the cart?
+          </h4>
+          <div className={cl.text__buttons_block}>
+            <Button isMain={true} onClick={clearBasket}>
+              Yes!
+            </Button>
+            <Button isMain={true} onClick={closeModal}>
+              No
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClearModal;

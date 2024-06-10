39a265b76e3f7b98ee4acd3876ea6f4025801cb4
D4n1el13and37 <daniel837@mail.ru>
2024-06-10 18:02:02 +0300

    refafctor: add basket content component at basket page instead of code

--- src/pages/Basket/BasketPage.tsx
+++ src/pages/Basket/BasketPage.tsx
@@ -1,34 +1,15 @@
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import BasketCard from './components/BasketCard';
+import BasketContent from './components/BasketContent/BasketContent';
 
-import cl from './BasketPage.module.scss';
-import EmptyCart from './components/empty/EmptyCart';
-
-const BasketPage = () => {
-  const mockArray = [];
-  return (
-    <>
-      <Header />
-      <main className="container">
-        <div className={cl.main__wrapper}>
-          <h1 className={cl.main__title}>Shopping Bag</h1>
-
-          {/* mock checker if cart is empty */}
-          {mockArray.length ? (
-            <>
-              <BasketCard />
-              <BasketCard />
-              <BasketCard />
-            </>
-          ) : (
-            <EmptyCart />
-          )}
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
-};
+const BasketPage = () => (
+  <>
+    <Header />
+    <main className="container">
+      <BasketContent />
+    </main>
+    <Footer />
+  </>
+);
 
 export default BasketPage;

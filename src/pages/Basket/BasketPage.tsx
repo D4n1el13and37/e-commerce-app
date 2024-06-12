2c8587c0d19619d92756f3a853911d8ac2661fbf
D4n1el13and37 <daniel837@mail.ru>
2024-06-12 17:14:57 +0300

    rfix: update basket card component

--- src/pages/Basket/BasketPage.tsx
+++ src/pages/Basket/BasketPage.tsx
@@ -1,18 +1,31 @@
-// import React from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import BasketCard from './components/BasketCard';
+import BasketCard from './components/basketCard/BasketCard';
+import useAppSelector from '../../hooks/useAppSelector';
 
-const BasketPage = () => (
-  <>
-    <Header />
-    <main>
-      <BasketCard />
-      <BasketCard />
-      <BasketCard />
-    </main>
-    <Footer />
-  </>
-);
+const BasketPage = () => {
+  const currentCartItems = useAppSelector((state) => state.cart.cartItems);
+
+  return (
+    <>
+      <Header />
+      <main>
+        {currentCartItems.map((card) => (
+          <BasketCard
+            key={card.id}
+            id={card.id}
+            productId={card.productId}
+            name={card.name['en-US']}
+            price={card.price}
+            image={card.variant.images!}
+            attributes={card.variant.attributes!}
+            totalPrice={card.totalPrice.centAmount}
+          />
+        ))}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default BasketPage;

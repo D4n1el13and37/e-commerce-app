930abc236f86684b294fb703ff2d1c77c9457232
D4n1el13and37 <daniel837@mail.ru>
2024-06-13 17:58:18 +0300

    feat: add full price at page, refactor styles

--- src/pages/Basket/BasketPage.tsx
+++ src/pages/Basket/BasketPage.tsx
@@ -2,12 +2,13 @@ import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import BasketCard from './components/basketCard/BasketCard';
 import useAppSelector from '../../hooks/useAppSelector';
-
 import cl from './BasketPage.module.scss';
 import EmptyCart from './components/empty/EmptyCart';
+import madeCorrectOutputPrice from '../../utils/madeCorrectOutputPrice';
 
 const BasketPage = () => {
-  const currentCartItems = useAppSelector((state) => state.cart.cartItems);
+  const { cartItems, cart } = useAppSelector((state) => state.cart);
+  const FULL_PRICE = madeCorrectOutputPrice(cart.totalPrice?.centAmount || 0);
 
   return (
     <>
@@ -15,19 +16,30 @@ const BasketPage = () => {
       <main className="container grid">
         <h1 className={cl.main__title}>Shopping Bag</h1>
         <div className={cl.content__wrapper}>
-          {currentCartItems.length ? (
-            currentCartItems.map((card) => (
-              <BasketCard
-                key={card.id}
-                id={card.id}
-                productId={card.productId}
-                name={card.name['en-US']}
-                price={card.price}
-                image={card.variant.images!}
-                attributes={card.variant.attributes!}
-                quantity={card.quantity}
-              />
-            ))
+          {cartItems.length ? (
+            <div className={cl.card_list__wrapper}>
+              {cartItems.map((card) => (
+                <BasketCard
+                  key={card.id}
+                  id={card.id}
+                  productId={card.productId}
+                  name={card.name['en-US']}
+                  price={card.price}
+                  image={card.variant.images!}
+                  attributes={card.variant.attributes!}
+                  quantity={card.quantity}
+                />
+              ))}
+              <>
+                <div className={cl.full_price__wrapper}>
+                  <span className={cl.full_price__wrapper_text}>Total</span>
+                  <hr className={cl.full_price__wrapper_dash} />
+                  <span className={cl.full_price__wrapper_text}>
+                    {FULL_PRICE}
+                  </span>
+                </div>
+              </>
+            </div>
           ) : (
             <EmptyCart />
           )}

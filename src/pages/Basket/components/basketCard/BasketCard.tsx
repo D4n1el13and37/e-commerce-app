5c61b10fd341ff676d674bce6f9b4bd0e8b450ea
D4n1el13and37 <daniel837@mail.ru>
2024-06-13 13:50:51 +0300

    feat: add goods count functionality

--- src/pages/Basket/components/basketCard/BasketCard.tsx
+++ src/pages/Basket/components/basketCard/BasketCard.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Attribute, Image, Price } from '@commercetools/platform-sdk';
+
+import Button from '../../../../components/ui/button/Button';
 import cl from './BasketCard.module.scss';
 import AttributesBlock from '../attributes/AttributesBlock';
+import useAppDispatch from '../../../../hooks/useAppDispatch';
+import { getChangeQuantity } from '../../../../store/cartSlice';
 import PriceBlock from '../price/PriceBlock';
 import madeCorrectOutputPrice from '../../../../utils/madeCorrectOutputPrice';
+// import madeCorrectOutputPrice from '../../../../utils/madeCorrectOutputPrice';
 
 export interface BasketItem {
   id: string;
@@ -12,31 +17,101 @@ export interface BasketItem {
   price: Price;
   image: Image[];
   attributes: Attribute[];
+  quantity: number;
   totalPrice: number;
 }
 const BasketCard: React.FC<BasketItem> = ({
+  id,
   productId,
   name,
   price,
   image,
   attributes,
-  totalPrice,
-}: BasketItem) => (
-  <div className={cl.card__wrapper}>
-    <div className={cl.left_side__wrapper}>
-      <img src={image[0].url} alt={image[0].label} />
-    </div>
-    <div className={cl.right_side__wrapper}>
-      <h4 className={cl.right_side__title}>{name}</h4>
-      <PriceBlock price={price} />
-      <span className={cl.price__total_by_product}>
-        Total Price: {madeCorrectOutputPrice(totalPrice)}
-      </span>
-      <div className={cl.right_side__attributes}>
-        <AttributesBlock attributes={attributes} id={productId} />
+  quantity,
+  // totalPrice,
+}: BasketItem) => {
+  const dispatch = useAppDispatch();
+  const [count, setCount] = useState(quantity);
+  // const [totalProductPrice, setTotalProductPrice] = useState(totalPrice);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleChangeQuantity = (productQuantity: number) => {
+    dispatch(getChangeQuantity({ productId: id, quanity: productQuantity }));
+  };
+
+  const increment = () => {
+    if (count < 99) {
+      handleChangeQuantity(count + 1);
+      setCount(count + 1);
+    } else {
+      setErrorMessage('We dont have enought plant in stock');
+    }
+  };
+  const decrement = () => {
+    if (count > 1) {
+      handleChangeQuantity(count - 1);
+      setCount(count - 1);
+    } else {
+      setErrorMessage('Sorry but we cant take your plants L_L');
+    }
+  };
+
+  const DISCOUNT = price.discounted?.value.centAmount;
+
+  return (
+    <div className={cl.card__wrapper}>
+      <div className={cl.left_side__wrapper}>
+        <img src={image[0].url} alt={image[0].label} />
+      </div>
+      <div className={cl.right_side__wrapper}>
+        <div className={cl.right_side__headline}>
+          <h4 className={cl.right_side__title}>{name}</h4>
+        </div>
+        <PriceBlock price={price} />
+
+        <div className={cl.right_side__attributes}>
+          <AttributesBlock attributes={attributes} id={productId} />
+        </div>
+        <div className={cl.right_side__goods_controller}>
+          <div className={cl.right_side__goods_controller__block}>
+            <Button onClick={decrement}>-</Button>
+            <div className={cl.right_side__goods_controller__total_count}>
+              {count}
+            </div>
+            <Button onClick={increment}>+</Button>
+          </div>
+          <div className={cl.right_side__total_wrapper}>
+            <strong className={cl.right_side__total}>Total Price:</strong>
+            <div className={cl.price__total_by_product}>
+              {DISCOUNT ? (
+                <>
+                  <span className={cl.price__old}>
+                    {madeCorrectOutputPrice(count * price.value.centAmount!)}
+                  </span>
+                  <span>
+                    {madeCorrectOutputPrice(
+                      count * Number(price.discounted?.value.centAmount) || 0
+                    )}
+                  </span>
+                </>
+              ) : (
+                <span>
+                  {madeCorrectOutputPrice(
+                    count * Number(price.discounted?.value.centAmount) || 0
+                  )}
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+        {errorMessage && (
+          <div className={cl.right_side__goods_controller__error}>
+            {errorMessage}
+          </div>
+        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BasketCard;

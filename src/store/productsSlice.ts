5b0714fa892fb012682e0d3be52670e1d85c32bc
Mineclinee <vasily-minecline@yandex.ru>
2024-06-15 12:12:52 +0300

    refactor: change styles for search element

--- src/store/productsSlice.ts
+++ src/store/productsSlice.ts
@@ -309,18 +309,19 @@ const productsSlice = createSlice({
         const newState = state;
         newState.isLoading = false;
       })
-      .addCase(fetchSearchProducts.pending, (state) => {
-        const newState = state;
-        newState.isLoading = true;
-      })
-      .addCase(fetchSearchProducts.fulfilled, (state) => {
-        const newState = state;
-        newState.isLoading = false;
-      })
-      .addCase(fetchSearchProducts.rejected, (state) => {
-        const newState = state;
-        newState.isLoading = false;
-      })
+      // ? Loader when we use search
+      // .addCase(fetchSearchProducts.pending, (state) => {
+      //   const newState = state;
+      //   newState.isLoading = true;
+      // })
+      // .addCase(fetchSearchProducts.fulfilled, (state) => {
+      //   const newState = state;
+      //   newState.isLoading = false;
+      // })
+      // .addCase(fetchSearchProducts.rejected, (state) => {
+      //   const newState = state;
+      //   newState.isLoading = false;
+      // })
       .addCase(fetchProduct.pending, (state) => {
         const newState = state;
         newState.isLoading = true;

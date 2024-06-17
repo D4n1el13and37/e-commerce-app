ed3a98191e00c2d4b052aaa70a3707bfb0a04fff
D4n1el <87085006+D4n1el13and37@users.noreply.github.com>
2024-06-17 18:31:13 +0300

    feat: Message For Empty Cart (#85)
    * refactor: change product card structure
    
    * feat: create a simple product card
    
    * feat: create basket page, add cards into it
    
    * feat: add basket page at routing
    
    * feat: add empty cart component, implement styles at catalog page
    
    * feat: add loading, adding to cart, store for cart, cart methods api, cartSlice, animation for loader, small changes
    
    * refactor: remove unnecessary code in loader
    
    * feat: add price block component
    
    * feat: add attribute block component
    
    * feat: update basket component (get data from API) add price and attribute blocks
    
    * feat: add cartItems state
    
    * feat: add folder for basketCard component
    
    * feat: add correct output price util's function
    
    * rfix: update basket card component
    
    * fix: update path to basket page at header
    
    * fix: fix price block
    
    * feat: add goods count functionality
    
    * fix: update basket page
    
    * fix: add list product dependency to handle counting
    
    * fix: remove unnecessary total price of products(we count it via price and quantity)
    
    * feat: add remove card functionality remove unnecessary 'count' state
    
    * fix: styles(add grid column property and wrap content)
    
    * refactor: remove unused file
    
    ---------
    
    Co-authored-by: Mineclinee <vasily-minecline@yandex.ru>

--- src/pages/Basket/BasketPage.tsx
+++ src/pages/Basket/BasketPage.tsx
@@ -3,30 +3,38 @@ import Footer from '../../components/footer/Footer';
 import BasketCard from './components/basketCard/BasketCard';
 import useAppSelector from '../../hooks/useAppSelector';
 
+import cl from './BasketPage.module.scss';
+import EmptyCart from './components/empty/EmptyCart';
+
 const BasketPage = () => {
   const currentCartItems = useAppSelector((state) => state.cart.cartItems);
 
   return (
     <>
       <Header />
-      {/* <main className="container"> */}
-      <main>
-        {currentCartItems.map((card) => (
-          <BasketCard
-            key={card.id}
-            id={card.id}
-            productId={card.productId}
-            name={card.name['en-US']}
-            price={card.price}
-            image={card.variant.images!}
-            attributes={card.variant.attributes!}
-            quantity={card.quantity}
-          />
-        ))}
+      <main className="container grid">
+        <h1 className={cl.main__title}>Shopping Bag</h1>
+        <div className={cl.content__wrapper}>
+          {currentCartItems.length ? (
+            currentCartItems.map((card) => (
+              <BasketCard
+                key={card.id}
+                id={card.id}
+                productId={card.productId}
+                name={card.name['en-US']}
+                price={card.price}
+                image={card.variant.images!}
+                attributes={card.variant.attributes!}
+                quantity={card.quantity}
+              />
+            ))
+          ) : (
+            <EmptyCart />
+          )}
+        </div>
       </main>
       <Footer />
     </>
   );
 };
-
 export default BasketPage;

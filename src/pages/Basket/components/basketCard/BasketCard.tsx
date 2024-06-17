994f484ad3bd8d2d489a89eb9d98c26f765681b1
Mineclinee <vasily-minecline@yandex.ru>
2024-06-17 21:41:00 +0300

    feat: add adaptive layout

--- src/pages/Basket/components/basketCard/BasketCard.tsx
+++ src/pages/Basket/components/basketCard/BasketCard.tsx
@@ -5,7 +5,7 @@ import {
   Image,
   Price,
 } from '@commercetools/platform-sdk';
-
+import { CSSTransition } from 'react-transition-group';
 import Button from '../../../../components/ui/button/Button';
 import cl from './BasketCard.module.scss';
 import AttributesBlock from '../attributes/AttributesBlock';
@@ -41,6 +41,7 @@ const BasketCard: React.FC<BasketItem> = ({
 
   const handleChangeQuantity = (productQuantity: number) => {
     dispatch(getChangeQuantity({ productId: id, quantity: productQuantity }));
+    setErrorMessage(null);
   };
 
   const handleAddProduct = () => {
@@ -55,7 +56,7 @@ const BasketCard: React.FC<BasketItem> = ({
     if (quantity > 1) {
       handleChangeQuantity(quantity - 1);
     } else {
-      setErrorMessage('Sorry but we cant take your plants L_L');
+      setErrorMessage("Sorry but we can't take your plants");
     }
   };
 
@@ -76,7 +77,15 @@ const BasketCard: React.FC<BasketItem> = ({
       <div className={cl.right_side__wrapper}>
         <div className={cl.right_side__headline}>
           <h4 className={cl.right_side__title}>{name}</h4>
-          <Button onClick={handleRemoveProduct}>x</Button>
+          <Button onClick={handleRemoveProduct}>
+            <svg
+              fill="#758963"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <polygon points="24 1.414 22.586 0 12 10.586 1.414 0 0 1.414 10.586 12 0 22.586 1.414 24 12 13.414 22.586 24 24 22.586 13.414 12 24 1.414" />
+            </svg>
+          </Button>
         </div>
         <PriceBlock price={price} />
 
@@ -85,11 +94,32 @@ const BasketCard: React.FC<BasketItem> = ({
         </div>
         <div className={cl.right_side__goods_controller}>
           <div className={cl.right_side__goods_controller__block}>
-            <Button onClick={handleDecreaseProduct}>-</Button>
+            <Button onClick={handleDecreaseProduct}>
+              <svg
+                width="15"
+                height="3"
+                viewBox="0 0 15 3"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 1.5H15" stroke="#758963" stroke-width="1.5" />
+              </svg>
+            </Button>
             <div className={cl.right_side__goods_controller__total_count}>
               {quantity}
             </div>
-            <Button onClick={handleAddProduct}>+</Button>
+            <Button onClick={handleAddProduct}>
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 7.5H15" stroke="#758963" stroke-width="1.5" />
+                <path d="M7.5 0L7.5 15" stroke="#758963" stroke-width="1.5" />
+              </svg>
+            </Button>
           </div>
           <div className={cl.right_side__total_wrapper}>
             <strong className={cl.right_side__total}>Total Price:</strong>
@@ -111,11 +141,16 @@ const BasketCard: React.FC<BasketItem> = ({
             </div>
           </div>
         </div>
-        {errorMessage && (
+        <CSSTransition
+          in={!!errorMessage}
+          classNames="clear"
+          timeout={300}
+          unmountOnExit
+        >
           <div className={cl.right_side__goods_controller__error}>
             {errorMessage}
           </div>
-        )}
+        </CSSTransition>
       </div>
     </div>
   );
